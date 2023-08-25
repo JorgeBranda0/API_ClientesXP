41e001074df46dd120595cb8323cf08f1bc3d39b
Jorge Brandão <brandaoneto01@gmail.com>
2023-08-25 00:01:14 -0300

    alterações para o docker-compose

--- XP_TesteTecnico/init.sql
+++ XP_TesteTecnico/init.sql
@@ -1,20 +1,29 @@
 USE master;
+GO
+
 CREATE DATABASE BDadosXP;
+GO
 
-CREATE TABLE BDadosXP.dbo.Nome    ( NomeId       INT           NOT NULL   IDENTITY    ( 1,1        ),
-						            NomeCompleto VARCHAR( 50 ) NOT NULL
-												 CONSTRAINT PK_NomeId     PRIMARY KEY ( NomeId     ) )
+USE BDadosXP;
+GO
 
-CREATE TABLE BDadosXP.dbo.Telefone( TelefoneId   INT           NOT NULL   IDENTITY    ( 1,1        ),
-									Telefone     VARCHAR( 50 ) NOT NULL 
-												 CONSTRAINT PK_TelefoneId PRIMARY KEY ( TelefoneId ) )
+CREATE TABLE Nome ( NomeId       INT		   NOT NULL IDENTITY       ( 1,1        ),
+					NomeCompleto VARCHAR( 50 ) NOT NULL
+							     CONSTRAINT PK_NomeId   PRIMARY KEY    ( NomeId     ) );
+GO
 
-CREATE TABLE BDadosXP.dbo.Email   ( EmailId      INT           NOT NULL   IDENTITY    ( 1,1        ),
-									Email        VARCHAR( 50 ) NOT NULL 
-												 CONSTRAINT PK_EmailId    PRIMARY KEY ( EmailId    ) )
+CREATE TABLE Telefone( TelefoneId INT           NOT NULL   IDENTITY    ( 1,1        ),
+					   Telefone   VARCHAR( 50 ) NOT NULL 
+								  CONSTRAINT PK_TelefoneId PRIMARY KEY ( TelefoneId ) );
+GO
 
-CREATE TABLE BDadosXP.dbo.DadosCliente ( ClienteId  INT NOT NULL IDENTITY                ( 1, 1       ),
-										 NomeId     INT NOT NULL REFERENCES Nome         ( NomeId     ), 
-										 TelefoneId INT NOT NULL REFERENCES Telefone     ( TelefoneId ),
-										 EmailId    INT NOT NULL REFERENCES Email        ( EmailId    )
-                                                    CONSTRAINT PK_ClientelId PRIMARY KEY ( ClienteId  ) )
+CREATE TABLE Email ( EmailId INT           NOT NULL IDENTITY           ( 1,1        ),
+					 Email   VARCHAR( 50 ) NOT NULL 
+			                 CONSTRAINT PK_EmailId  PRIMARY KEY        ( EmailId    ) );
+GO
+
+CREATE TABLE DadosCliente ( ClienteId  INT NOT NULL IDENTITY                ( 1, 1       ),
+							NomeId     INT NOT NULL REFERENCES Nome         ( NomeId     ), 
+							TelefoneId INT NOT NULL REFERENCES Telefone     ( TelefoneId ),
+							EmailId    INT NOT NULL REFERENCES Email        ( EmailId    )
+									   CONSTRAINT PK_ClientelId PRIMARY KEY ( ClienteId  ) );
